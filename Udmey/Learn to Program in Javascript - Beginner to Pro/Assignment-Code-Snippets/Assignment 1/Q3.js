2eea089a6dcc0c332797c80475c2ca209ec61990
Arnie <59781783+Barani2396@users.noreply.github.com>
2020-05-09 23:40:21 -0400

    commits

--- Udmey/Learn to Program in Javascript - Beginner to Pro/Assignment-Code-Snippets/Assignment 1/Q3.js
+++
@@ -1,2 +0,0 @@
-/* Write a JavaScript program to convert temperatures to and from celsius, fahrenheit. 
-[ Use the formula : c/5 = (f-32)/9, where c = temperature in celsius and f = temperature in fahrenheit] */
---
+++ Udmey/Learn to Program in Javascript - Beginner to Pro/Assignment-Code-Snippets/Q5.js
@@ -0,0 +1,10 @@
+/* Write a javascript function that converts a given amount of money into coins of denominations (1, 2, 5, 10 and 25). 
+[function convert_to_coins(amount) {...}]. 
+You may choose to print the coin denominations used on the console. E.g. convert_to_coins(87) should print 25 25 25 10 2. */
+
+function convert_to_coins(amt) {
+    if (amt <= 5) {
+        amt
+    }
+
+}
