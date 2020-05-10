e497d960b395ab751069cece87b2e460a77e65b3
Arnie <59781783+Barani2396@users.noreply.github.com>
2020-05-09 20:13:41 -0400

    Initial commit

---
+++ Udmey/Learn to Program in Javascript - Beginner to Pro/Assignments-Code-Snippets/Assignment 1/Q3.js
@@ -0,0 +1,2 @@
+/* Write a JavaScript program to convert temperatures to and from celsius, fahrenheit. 
+[ Use the formula : c/5 = (f-32)/9, where c = temperature in celsius and f = temperature in fahrenheit] */
